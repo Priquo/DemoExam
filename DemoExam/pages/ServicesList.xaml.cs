631515b2859db880b2b9421e55c39ec1008e8cad
СтадниченкоАД <@TS-06>
2022-02-04 10:57:13 +0300

    Добавьте файлы проекта.

---
+++ DemoExam/pages/ServicesList.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DemoViewModel;
+
+namespace DemoExam.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ServicesList.xaml
+    /// </summary>
+    public partial class ServicesList : Page
+    {
+       
+        public ServicesList()
+        {
+            InitializeComponent();
+            LoadServices loadServices = new LoadServices();
+            listboxServices.ItemsSource = loadServices.Services;
+        }
+
+        private void listboxServices_Loaded(object sender, RoutedEventArgs e)
+        {
+            ListBox listBox = (ListBox)sender;
+
+        }
+    }
+}
---
+++ DemoViewModel/LoadServices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DemoViewModel
+{
+    public class LoadServices
+    {
+       
+        List<Services> services = BaseConnect.BaseModel.Services.ToList();
+        public List<Services> Services
+        {
+            get
+            {
+                return services;
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                DemoViewModel.Services s = new Services();
+                return s.Title;
+            }
+        }
+        public LoadServices()
+        {
+          //  BaseConnect.BaseModel = new Entities();
+        }
+
+        
+    }
+}
